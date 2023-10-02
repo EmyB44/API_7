0380f89573f5d082ee1afdc80ecc0f9f1642a190
EmyB44 <emilie.brosseau@gmail.com>
2023-10-02 10:53:36 +0200

    lancement

--- app.py
+++ app.py
@@ -83,5 +83,5 @@ def client_info(id_client):
         return str(e)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+#if __name__ == '__main__':
+ #   app.run(host='0.0.0.0', port=5000)
